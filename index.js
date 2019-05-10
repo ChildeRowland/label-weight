b6454fe0e02531ecf03a09c470616c4880039147
Chris Mehmed <chris.m@agencywithin.com>
2019-05-10 13:57:38 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+console.log('start');
+
+const topData = [
+	['2_people', 'man', 'woman'],
+	['2_people', 'man', 'woman'],
+	['2_people', 'woman']
+];
+
+const bottomData = [
+	['2_people', 'man'],
+	['2_people', 'man'],
+	['1_people', 'man']
+];
+
+function highestValueLabels (high, low) {
+
+	const labelMetricsArr = [];
+
+	for (let i = 0; i < high.length; i++) {
+
+		let labels = high[i];
+
+		console.log(high[i][0]);
+
+	}
+
+}
+
+highestValueLabels(topData, bottomData);
