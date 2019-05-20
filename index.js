f06b4bc40a44a4f921bcc6538ad9dcbfd79330b0
Chris Mehmed <chris.m@agencywithin.com>
2019-05-20 07:58:11 -0400

    combination method

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-console.log('start');
+
+const adIds = [1,2,3,4,5,6];
 
 const topData = [
 	['2_people', 'man', 'woman'],
@@ -12,18 +13,98 @@ const bottomData = [
 	['1_people', 'man']
 ];
 
-function highestValueLabels (high, low) {
+// find the most commonly occuring labels in the top ads
+function commonOccuringLabels (labelMatrix) {
 
-	const labelMetricsArr = [];
+	const result = {};
 
-	for (let i = 0; i < high.length; i++) {
+	labelMatrix.forEach((labelArr) => {
 
-		let labels = high[i];
+		labelArr.forEach((label) => {
 
-		console.log(high[i][0]);
+			if (result[label]) {
+				result[label] += 1;
+				return;
+			}
 
-	}
+			result[label] = 1;
+
+		});
+
+	});
+
+	return result;
 
 }
 
-highestValueLabels(topData, bottomData);
+// find the most popular labels by percentage
+function labelPercentOccuring(labelMatrix) {
+
+	const result = {};
+
+	const commonLabelsObj = commonOccuringLabels(labelMatrix);
+
+	Object.keys(commonLabelsObj).forEach((label) => {
+
+		result[label] = Math.round(commonLabelsObj[label] / labelMatrix.length * 100);
+
+	});
+
+	return result;
+
+}
+
+// find reoccuring series of labels accross top ads
+function commonSeriesOfLabels(labelMatrix) {
+
+	// store the taco arr in an object with the number of times the values appear
+
+	const result = {};
+
+	labelMatrix.forEach((labelArr) => {
+
+		let labelCombinationMatrix = labelCombinations(labelArr);
+
+	});
+
+}
+
+// console.log('\n *** most commonly occuring labels ***');
+// console.log(commonOccuringLabels(topData));
+
+// console.log('\n *** percentage occuring labels ***');
+// console.log(labelPercentOccuring(topData));
+
+// console.log('\n *** common series of labels ***');
+// console.log(commonSeriesOfLabels([['2_people', 'man', 'woman', 'dog', 'cat']]));
+
+function labelCombinations( list ){
+  let set = [],
+      listSize = list.length,
+      combinationsCount = (1 << listSize);
+
+  for (let i = 1; i < combinationsCount ; i++ ){
+	  
+	  let combination = [];
+	  
+	  for (let j = 0; j < listSize; j++){
+      if ((i & (1 << j))){
+        combination.push(list[j]);
+      }
+	  }
+
+	  if (combination.length > 1) {
+	  	set.push(combination);
+	  }
+
+  }
+
+  return set;
+}
+
+var labelCombinations = powerSet(['2_people', 'man', 'woman', 'dog', 'cat']);
+
+console.log(combinations);
+
+
+
